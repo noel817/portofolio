e5b791016ad550d44c363231a2d35bc3bd4d2c0e
andres sanoel sianipar xi pplg <andresanoel183@gmail.com>
2026-09-14 10:30:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const navToggle = document.getElementById('navToggle');
+const navLinks = document.getElementById('navLinks');
+
+if (navToggle && navLinks) {
+  navToggle.addEventListener('click', () => {
+    const isOpen = navLinks.classList.toggle('open');
+    navToggle.setAttribute('aria-expanded', isOpen);
+  });
+  navLinks.querySelectorAll('a').forEach(a => {
+    a.addEventListener('click', () => {
+      navLinks.classList.remove('open');
+      navToggle.setAttribute('aria-expanded', 'false');
+    });
+  });
+}
